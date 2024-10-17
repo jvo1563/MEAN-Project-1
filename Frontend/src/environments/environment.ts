d0b60be12a3a82f3b3600f150ba263b76203c91d
jvo1563 <75157418+jvo1563@users.noreply.github.com>
2024-10-16 18:00:00 -0700

    testing ngrok

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://454c-12-74-244-73.ngrok-free.app/api',
+  apiUrl: process.env['backend'] || 'https://6e9a-12-74-244-73.ngrok-free.app',
 };
