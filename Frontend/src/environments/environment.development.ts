3aaf52f3db94a4ff054a4f241c026d6e716c7d36
jvo1563 <75157418+jvo1563@users.noreply.github.com>
2024-10-17 12:28:05 -0700

    changed frontend to used ec2 in prod

--- Frontend/src/environments/environment.development.ts
+++ Frontend/src/environments/environment.development.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000/api',
   // apiUrl: 'https://mean-project-1-a0j5.onrender.com/api',
+  // apiUrl: 'http://98.81.172.62/api',
 };
--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://mean-project-1-a0j5.onrender.com/api',
+  // apiUrl: 'https://mean-project-1-a0j5.onrender.com/api',
+  apiUrl: 'http://98.81.172.62/api',
 };
