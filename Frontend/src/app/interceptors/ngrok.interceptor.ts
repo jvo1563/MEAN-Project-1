70d1939949fd5dc336cac479808bb2adfba64f6b
jvo1563 <75157418+jvo1563@users.noreply.github.com>
2024-10-16 18:26:04 -0700

    working on interceptor

--- Frontend/src/app/interceptors/ngrok.interceptor.ts
+++ Frontend/src/app/interceptors/ngrok.interceptor.ts
@@ -4,7 +4,7 @@ import { environment } from '../../environments/environment';
 export const ngrokInterceptor: HttpInterceptorFn = (req, next) => {
   if (environment.production) {
     let newReq = req.clone({
-      headers: req.headers.set('ngrok-skip-browser-warning', ''),
+      headers: req.headers.set('ngrok-skip-browser-warning', 'let me IN'),
     });
     return next(newReq);
   }
