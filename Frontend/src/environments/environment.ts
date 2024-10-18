b48485687b1db6a5f3e28fb91fe713fdaf6b8b60
jvo1563 <75157418+jvo1563@users.noreply.github.com>
2024-10-18 14:34:20 -0700

    changed prod to use aws ec2 backend

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://mean-project-1-a0j5.onrender.com/api',
+  // apiUrl: 'https://mean-project-1-a0j5.onrender.com/api',
   // apiUrl: 'http://98.81.172.62/api',
+  apiUrl: 'https://api.joeny.xyz/api',
 };
